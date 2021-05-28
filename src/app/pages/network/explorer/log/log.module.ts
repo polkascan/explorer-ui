ac60b328161e6d33f3e490e3bfb175005f0a4e37
michielvandergraaf <michiel@kriekkop.nl>
2021-05-28 15:17:12 +0200

    Log detail updated

--- src/app/pages/network/explorer/log/log.module.ts
+++ src/app/pages/network/explorer/log/log.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { LogRoutingModule } from './log-routing.module';
 import { LogDetailComponent } from './log-detail/log-detail.component';
 import { LogListComponent } from './log-list/log-list.component';
+import { PolkadotAngularLibModule } from '../../../../../substrate-components-lib/polkadot-angular-lib.module';
 
 
 @NgModule({
   declarations: [LogDetailComponent, LogListComponent],
   imports: [
     CommonModule,
-    LogRoutingModule
+    LogRoutingModule,
+    PolkadotAngularLibModule
   ]
 })
 export class LogModule { }
